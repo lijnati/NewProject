5d204557ed97ac5f5be8dbdcb25c513d344c73d6
ceoyegetaneh <lijnati3@gmail.com>
2022-11-30 23:19:54 +0400

    file

---
+++ app.js
@@ -0,0 +1,6 @@
+const wrapper = document.querySelector(".wrapper"),
+generateBtn =  wrapper.querySelector(".form button");
+
+generateBtn.addEventListener("click", ()=> {
+  wrapper.classList.add("active");
+});
